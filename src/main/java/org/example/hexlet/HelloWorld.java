1a727fe6a8f67cef04f962b228a555807cb77d9d
Anton Romanovsky <romcky@gmail.com>
2024-03-29 11:15:38 +0300

    Hello Controller

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -3,11 +3,21 @@ package org.example.hexlet;
 import io.javalin.Javalin;
 
 public class HelloWorld {
-    public static void main(String[] args) {
-        var app = Javalin.create(javalinConfig -> {
-           javalinConfig.bundledPlugins.enableDevLogging();
+
+    public static Javalin getHello() {
+        Javalin app = Javalin.create();
+        app.get("/hello", ctx -> {
+            String name = ctx.queryParam("name");
+            ctx.contentType("text/html");
+            String body = String.format("<h1>Hello, %s!</h1>",
+                    name == null ? "World" : name);
+            ctx.result(body);
         });
-        app.get("/", ctx -> ctx.result("Hello World"));
+        return app;
+    }
+
+    public static void main(String[] args) {
+        var app = getHello();
         app.start(7070);
     }
 }
