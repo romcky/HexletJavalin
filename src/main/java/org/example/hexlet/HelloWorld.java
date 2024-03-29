7ba9cc2bb3e998c73a02fe8c9003445473e030d6
Anton Romanovsky <romcky@gmail.com>
2024-03-29 11:48:58 +0300

    path request

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -1,9 +1,23 @@
 package org.example.hexlet;
 
 import io.javalin.Javalin;
+import io.javalin.http.NotFoundResponse;
 
 public class HelloWorld {
 
+    public static Javalin getUsers() {
+        Javalin app = Javalin.create();
+        app.get("/users/{id}/post/{postId}", ctx -> {
+            try {
+                int id = ctx.pathParamAsClass("id", Integer.class).get();
+                int postId = ctx.pathParamAsClass("postId", Integer.class).get();
+                ctx.result(String.format("id = %d, postId = %d", id, postId));
+            } catch (Exception exception) {
+                ctx.result("Wrong id or postId!");
+            }
+        });
+        return app;
+    }
     public static Javalin getHello() {
         Javalin app = Javalin.create();
         app.get("/hello", ctx -> {
@@ -17,7 +31,7 @@ public class HelloWorld {
     }
 
     public static void main(String[] args) {
-        var app = getHello();
+        var app = getUsers();
         app.start(7070);
     }
 }
